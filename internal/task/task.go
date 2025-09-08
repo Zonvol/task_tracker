10ee4c9dce3a828d02bf6bc1e0f3b4f0876c953e
Kirill Shevchik <kirillshevchik10@gmail.com>
2025-09-08 15:36:28 +0300

    modified sturct of proj

--- internal/task/task.go
+++ internal/task/task.go
@@ -7,23 +7,3 @@ type Task struct {
     Status      string
 }
 
-func NewTask(id int, title, description, status string) *Task {
-    return &Task{
-        ID:          id,
-        Title:       title,
-        Description: description,
-        Status:      status,
-    }
-}
-
-func (t *Task) UpdateTitle(title string) {
-    t.Title = title
-}
-
-func (t *Task) UpdateDescription(description string) {
-    t.Description = description
-}
-
-func (t *Task) UpdateStatus(status string) {
-    t.Status = status
-}
